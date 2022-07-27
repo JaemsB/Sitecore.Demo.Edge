55ddc569c19dd03fa41041df55dfbe95dc633d85
JaemsB <102551868+JaemsB@users.noreply.github.com>
2022-07-26 17:27:24 -0700

    Update _app.tsx
    removed attempted moosend tag which was causing compile to fail

--- Website/src/rendering/src/pages/_app.tsx
+++ Website/src/rendering/src/pages/_app.tsx
@@ -81,15 +81,6 @@ function App({ Component, pageProps, router }: AppPropsWithLayout): JSX.Element 
       {CdpScripts}
       {/* END CUSTOMIZATION*/}
       
-      {/* added by Jamesb */}             
-<Script id="mootracker" strategy="afterInteractive">
-//load TrackerJS
-!function(t,n,e,o,a){function d(t){var n=~~(Date.now()/3e5),o=document.createElement(e);o.async=!0,o.src=t+"?ts="+n;var a=document.getElementsByTagName(e)[0];a.parentNode.insertBefore(o,a)}t.MooTrackerObject=a,t[a]=t[a]||function(){return t[a].q?void t[a].q.push(arguments):void(t[a].q=[arguments])},window.attachEvent?window.attachEvent("onload",d.bind(this,o)):window.addEventListener("load",d.bind(this,o),!1)}(window,document,"script","//cdn.stat-track.com/statics/moosend-tracking.min.js","mootrack");
-//tracker has to be initialized otherwise it will generate warnings and wont sendtracking events
-mootrack('init', '53edd770-3053-4e34-bc81-3d6c4224ebac');
-mootrack('trackPageView');
-console.log("mootrack called");
-</Script>
 
       {/*
         Use the next-localization (w/ rosetta) library to provide our translation dictionary to the app.
